fab6f7c4d685e5e8789257319b821e7362a1ded5
M. Nabil Adani <nblid48@gmail.com>
2022-11-22 19:40:44 +0700

    feat :add telescope hl-group

--- lua/ayu/plugins/telescope.lua
+++ lua/ayu/plugins/telescope.lua
@@ -1,6 +1,22 @@
 local c = require("ayu.colors")
 
 local theme = {
+	TelescopeSelection = { bg = c.selection, fg = c.fg, bold = true },
+	TelescopeSelectionCaret = { fg = c.func, bg = c.selection },
+	TelescopeMultiSelection = { fg = c.func, bg = c.none, bold = true },
+	TelescopeNormal = { bg = c.bg, fg = c.fg },
+
+	-- -- Border highlight groups
+	TelescopeBorder = { fg = c.fg_idle },
+	TelescopePreviewBorder = { fg = c.fg_idle },
+	TelescopePromptBorder = { fg = c.fg_idle },
+	TelescopeResultsBorder = { fg = c.fg_idle },
+	--
+	-- -- Color the prompt prefix
+	TelescopePromptPrefix = { fg = c.func },
+	--
+	-- -- Highlight characters your input matches
+	TelescopeMatching = { fg = c.func, bold = true },
 }
 
 return theme
